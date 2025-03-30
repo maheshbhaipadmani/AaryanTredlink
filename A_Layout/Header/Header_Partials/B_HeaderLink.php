9f81355b105ebc644d35680b2adb5c5fe3babb7f
patel-hitixa <hitixa.bhuva@theuniqueitsolution.com>
2025-03-30 14:31:26 +0530

    font family phone

--- A_Layout/Header/Header_Partials/B_HeaderLink.php
+++ A_Layout/Header/Header_Partials/B_HeaderLink.php
@@ -11,6 +11,7 @@ echo
     <!-- color scheme -->
     <link id="colors" href="css/colors/scheme-01.css" rel="stylesheet" type="text/css">
     <link id="colors" href="css/sitemap.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;700&display=swap" rel="stylesheet">
     <!-- Load non-critical CSS asynchronously -->
 
 
